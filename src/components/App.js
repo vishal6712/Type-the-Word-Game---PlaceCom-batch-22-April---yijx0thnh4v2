b954e98b140e3d46b7954704dd720cc3c106fefa
Vishal Kadam <79629824+vishal6712@users.noreply.github.com>
2023-04-22 20:17:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,28 @@ function App() {
   const [userInput, setUserInput] = useState('');
   const [result, setResult] = useState('');
   const [index, setIndex] = useState(0);
+  useEffect(()=>{
+  const timeout=setTimeout(()=>{
+  setFlashWord(false);},500);
+  return () => clearTimeout(timeout);},[word]);
+  
+  useEffect(()=>{
+  setWord(WORD_LIST[index]);
+  setFlashWord(true);
+  setUserInput('');
+  setResult('');},[index]);
+  
+  const handleInputChange=(event)=>{
+  setUserInput(event.target.value);};
+  
+  const handleFormSubmit=(event)=>{
+  event.preventDefault();
+  if(userInput.toLowerCase()===word.toLowerCae()){setResult('You won!');}
+  else{setResult('You lost!');}};
+  
+  const handleRestartClick= () =>{
+  if(index===WORD_LIST.length-1){setIndex(0);}
+  else{setIndex(index+1);}};
 
 
   return (
